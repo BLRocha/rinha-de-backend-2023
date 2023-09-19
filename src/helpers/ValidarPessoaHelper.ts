98dbbf2a39f1547ac2c8144ff55d67523b73591d
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2023-09-19 17:05:23 -0300

    fix: check error

--- src/helpers/ValidarPessoaHelper.ts
+++ src/helpers/ValidarPessoaHelper.ts
@@ -6,15 +6,15 @@ type Pessoa = {
 }
 
 const validarData = (dateString: string) => isNaN(new Date(dateString).getTime());
-const type = (a: any) => typeof a !== 'string';
+const type = (param: any) => /\d|null|false|true|undefined/.test(param);
 
 export const validarPessoaHelper = (pessoa: Pessoa | unknown) => {
     const { apelido, nome, nascimento, stack } = pessoa as Pessoa;
-    if (type(apelido) || apelido.length > 32)
+    if ( type(apelido) || apelido.length > 32 )
         return false;
     if (typeof stack === 'string' || typeof stack == 'number')
         return false
-    if ( type(nome) || nome.length > 100)
+    if ( type(nome) || nome.length > 100 )
         return false;
     if (validarData(nascimento)) return false;
     return (stack == null || stack.every( stkItem => (stkItem.length < 33 && !/\d|null|false|true|undefined/.test(stkItem))))
